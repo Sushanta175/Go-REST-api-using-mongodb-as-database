7bb4e5159ca7f2c9b59881ed789ef6b90f8e1029
Sushanta Sahu <sushantasahu175@gmail.com>
2024-11-04 18:17:13 +0530

    created a usercontroller

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"net/http"
 
+	"github.com/Sushanta175/Go-REST-api-using-mongodb-as-database/controllers"
 	"github.com/julienschmidt/httprouter"
 	"gopkg.in/mgo.v2"
 )
 
 func main() {
+	uc := controllers.NewUserController(getSession())
 	r := httprouter.New()
 	r.GET("/user/:id", uc.GetUser)
 	r.POST("/user", uc.CreateUser)
